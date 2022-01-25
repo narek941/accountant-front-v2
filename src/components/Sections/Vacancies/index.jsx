c127e0a65c6f6cac99f16a1a078827707dd46c50
Narek Kolyan <nqolyan@gmail.com>
2022-01-25 18:18:01 +0400

    adding vacancies section

--- src/components/Sections/Vacancies/index.jsx
+++ src/components/Sections/Vacancies/index.jsx
@@ -1,32 +1,38 @@
 import React from 'react';
-// import Slider from 'react-slick';
+import Slider from 'react-slick';
 
-// import Slide from './Slide';
+import { vacanciesList } from 'utils/index';
+
 import styles from './Vacancies.scss';
-// import NextArrow from './NextArrow';
-// import PrevArrow from './PrevArrow';
+import NextArrow from './NextArrow';
+import Slide from './Slide';
+import PrevArrow from './PrevArrow';
+
+import Button from '../../Button';
 
 const VacanciesSection = () => {
-  // const settings = {
-  //   dots: false,
-  //   speed: 500,
-  //   infinite: true,
-  //   className: styles.slider,
-  //   nextArrow: <NextArrow />,
-  //   prevArrow: <PrevArrow />,
-  //   slidesToShow: 2,
-  //   slidesToScroll: 1,
-  // };
+  const settings = {
+    dots: false,
+    speed: 500,
+    infinite: true,
+    className: styles.slider,
+    nextArrow: <NextArrow />,
+    prevArrow: <PrevArrow />,
+    slidesToShow: 4,
+    slidesToScroll: 1,
+  };
 
-  // const renderNewsList = newsList.map(({ id, img, text }) => (
-  //   <Slide id={id} img={img} text={text} />
-  // ));
+  const renderVacanciesList = vacanciesList.map(
+    ({ id, Icon, title, textSteps }) => (
+      <Slide key={id} Icon={Icon} title={title} textSteps={textSteps} />
+    ),
+  );
 
   return (
-    <div>
+    <div className="container">
       <h2 className={styles.title}>Թափուր հաստիքներ</h2>
-      <div />
-      {/* <Slider {...settings}>{renderNewsList}</Slider>  */}
+      <Slider {...settings}>{renderVacanciesList}</Slider>
+      <Button className={styles.button}>Դիմել</Button>
     </div>
   );
 };
