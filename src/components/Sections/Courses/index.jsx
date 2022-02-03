bbe6277c56045bad7ce4115d325245942091e164
Narek Kolyan <nqolyan@gmail.com>
2022-02-03 19:07:30 +0400

    creating Course Section

--- src/components/Sections/Courses/index.jsx
+++ src/components/Sections/Courses/index.jsx
@@ -1,19 +1,107 @@
-import React from 'react';
+import React, { useState } from 'react';
+import classNames from 'classnames';
 
 import { useToggle } from 'hooks/index';
 
 import styles from './Courses.scss';
 import CoursesForm from './CoursesForm';
+import TabContent from './TabContent';
 
 import Button from '../../Button';
+import { coursesNavigationList } from '../../../utils/index';
+import {
+  GroupIcon,
+  PersonalIcon,
+  PriceIcon,
+  TimeIcon,
+  LessonsIcon,
+} from '../../../icons';
 
 const CoursesSection = () => {
   const [isOpen, setIsOpen] = useToggle(false);
-
+  const [active, setActive] = useState(0);
+  const [isGroup, setIsGroup] = useState(false);
+  const handleToggle = () => {
+    setIsGroup(!isGroup);
+  };
+  const sellectedCourse = isGroup
+    ? coursesNavigationList[active].groupCourse
+    : coursesNavigationList[active].personalCourse;
   return (
     <div className={`container ${styles.wrapper}`}>
       <h2 className={styles.wrapper__title}>Դասընթացներ</h2>
-      {isOpen ? <CoursesForm /> : <Button onClick={setIsOpen}>Գրանցվել</Button>}
+      {isOpen ? (
+        <CoursesForm />
+      ) : (
+        <div className={styles.tabContainer}>
+          <div className={styles.tabContainer__types}>
+            {coursesNavigationList.map((type, index) => (
+              <div
+                className={classNames(styles.tabContainer__types_tabs, {
+                  [styles.tabContainer__types_tabs_active]: active === index,
+                })}
+                key={type.id}
+                role="button"
+                // active={active === index}
+                onClick={() => setActive(index)}
+              >
+                <type.Icon />
+                <span>{type.title}</span>
+              </div>
+            ))}
+          </div>
+
+          <div className={styles.tabContainer__content}>
+            <div className={styles.content__tabs}>
+              <div
+                role="button"
+                onClick={handleToggle}
+                className={classNames(styles.content__tabs_btn, {
+                  [styles.content__tabs_btn_active]: isGroup,
+                })}
+              >
+                <GroupIcon />
+                <p>Խմբակային</p>
+              </div>
+              <div
+                className={classNames(styles.content__tabs_btn, {
+                  [styles.content__tabs_btn_active]: !isGroup,
+                })}
+                role="button"
+                onClick={handleToggle}
+              >
+                <PersonalIcon />
+                <p>Անհատական</p>
+              </div>
+            </div>
+            <TabContent
+              infoSteps={coursesNavigationList[active].infoSteps}
+              course={sellectedCourse}
+            />
+          </div>
+
+          <div className={styles.tabContainer__priceList}>
+            <div className={styles.tabContainer__priceList_item}>
+              <TimeIcon />
+              <span>{sellectedCourse?.period}</span>
+            </div>
+            <div className={styles.tabContainer__priceList_item}>
+              <LessonsIcon />
+              <span>{sellectedCourse?.lessons}</span>
+            </div>
+            <div className={styles.tabContainer__priceList_item}>
+              <PriceIcon />
+              <span>{sellectedCourse?.price}</span>
+            </div>
+            <Button
+              onClick={setIsOpen}
+              className={styles.tabContainer__priceList_login}
+            >
+              Գրանցվել
+            </Button>
+          </div>
+        </div>
+      )}
     </div>
   );
 };
--- src/icons/index.js
+++ src/icons/index.js
@@ -3,12 +3,17 @@ export * from './vacancies';
 
 export { default as FbIcon } from './Fb.svg';
 export { default as LogoIcon } from './Logo.svg';
+export { default as TimeIcon } from './Time.svg';
 export { default as MailIcon } from './Mail.svg';
+export { default as PriceIcon } from './Price.svg';
 export { default as ChartIcon } from './Chart.svg';
 export { default as ExcelIcon } from './Excel.svg';
 export { default as ArrowIcon } from './Arrow.svg';
 export { default as PhoneIcon } from './Phone.svg';
+export { default as GroupIcon } from './Group.svg';
 export { default as SearchIcon } from './Search.svg';
 export { default as BurgerIcon } from './Burger.svg';
+export { default as LessonsIcon } from './Lessons.svg';
+export { default as PersonalIcon } from './Personal.svg';
 export { default as LinkedinIcon } from './Linkedin.svg';
 export { default as LocationIcon } from './Location.svg';
