a92c80d1a75f8ee6b9763662996824fa2417b24e
Narek Kolyan <nqolyan@gmail.com>
2022-02-09 17:45:19 +0400

    vacancies responsive

--- src/components/Sections/Vacancies/index.jsx
+++ src/components/Sections/Vacancies/index.jsx
@@ -24,6 +24,24 @@ const VacanciesSection = () => {
     prevArrow: <PrevArrow />,
     slidesToShow: 4,
     slidesToScroll: 1,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2,
+          rows: 2,
+        },
+      },
+      {
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1,
+          rows: 4,
+        },
+      },
+    ],
   };
 
   const renderVacanciesList = vacanciesList.map(
