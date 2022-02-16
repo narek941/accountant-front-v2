719d56453f60223b415df44bd71dab337fdc381c
Narek Kolyan <nqolyan@gmail.com>
2022-02-16 18:38:20 +0400

    fixing news component text style and newsList data

--- src/components/Sections/News/Slide.jsx
+++ src/components/Sections/News/Slide.jsx
@@ -3,21 +3,32 @@ import PropTypes from 'prop-types';
 
 import styles from './News.scss';
 
-const Slide = ({ id, img, title, text }) => (
-  <div key={id} className={styles.slider__item}>
-    <div className={styles.slider__item_wrapper}>
-      <p className={styles.slider__item_title}>
-        <img className={styles.slider__item_img} src={img} alt="img" /> {title}
-      </p>
-      <p className={styles.slider__item_text}>{text}</p>
+const Slide = ({ id, img, title, text }) => {
+  const render = text.map(({ subId, subTitle, subText }) => (
+    <span key={subId}>
+      <p>{subTitle}</p>
+      {subText?.map((i) => (
+        <p>🔹️{i}</p>
+      ))}
+    </span>
+  ));
+  return (
+    <div key={id} className={styles.slider__item}>
+      <div className={styles.slider__item_wrapper}>
+        <p className={styles.slider__item_title}>
+          <img className={styles.slider__item_img} src={img} alt="img" />
+          {title}
+        </p>
+        <div className={styles.slider__item_text}>{render}</div>
+      </div>
     </div>
-  </div>
-);
+  );
+};
 Slide.propTypes = {
   id: PropTypes.string,
   img: PropTypes.string,
   title: PropTypes.string,
-  text: PropTypes.string,
+  text: PropTypes.arrayOf(PropTypes.object),
 };
-Slide.defaultProps = { id: null, img: null, title: null, text: null };
+Slide.defaultProps = { id: null, img: null, title: null, text: [] };
 export default Slide;
