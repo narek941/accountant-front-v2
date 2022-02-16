fb16f66859887e398dbc80b042d93079c7f3ccc0
Narek Kolyan <nqolyan@gmail.com>
2022-02-16 17:21:50 +0400

    fixing news component style

--- src/components/Sections/News/Slide.jsx
+++ src/components/Sections/News/Slide.jsx
@@ -6,25 +6,18 @@ import styles from './News.scss';
 const Slide = ({ id, img, title, text }) => (
   <div key={id} className={styles.slider__item}>
     <div className={styles.slider__item_wrapper}>
-      <img className={styles.slider__item_img} src={img} alt="img" />
-      <p className={styles.slider__item_title}>{title}</p>
+      <p className={styles.slider__item_title}>
+        <img className={styles.slider__item_img} src={img} alt="img" /> {title}
+      </p>
+      <p className={styles.slider__item_text}>{text}</p>
     </div>
-    <p className={styles.slider__item_text}>{text}</p>
   </div>
 );
-
 Slide.propTypes = {
   id: PropTypes.string,
   img: PropTypes.string,
   title: PropTypes.string,
   text: PropTypes.string,
 };
-
-Slide.defaultProps = {
-  id: null,
-  img: null,
-  title: null,
-  text: null,
-};
-
+Slide.defaultProps = { id: null, img: null, title: null, text: null };
 export default Slide;
