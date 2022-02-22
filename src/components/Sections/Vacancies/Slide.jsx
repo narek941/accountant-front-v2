bca9216a3d99d61e2e4dbc0359d696f5c115c774
Narek Kolyan <nqolyan@gmail.com>
2022-02-22 14:57:12 +0400

    adding locales for all sections

--- src/components/Sections/Vacancies/Slide.jsx
+++ src/components/Sections/Vacancies/Slide.jsx
@@ -1,20 +1,24 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
+
+import { I18nContext } from 'context/index';
 
 import styles from './Vacancies.scss';
 
 const Slide = ({ Icon, title, textSteps }) => {
+  const t = useContext(I18nContext);
+
   const renderTextSteps = textSteps.map(({ id, text }, index) => (
     <p className={styles.slider__item_text} key={id}>
       <span className={styles.slider__item_text_order}>{index + 1}.</span>
-      {text}
+      {t(text)}
     </p>
   ));
 
   return (
     <div className={styles.slider__item}>
       <Icon />
-      <p className={styles.slider__item_title}>{title}</p>
+      <p className={styles.slider__item_title}>{t(title)}</p>
       <div className={styles.slider__item_wrapper}>{renderTextSteps}</div>
     </div>
   );
