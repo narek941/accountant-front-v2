be0b08c33f5552dd0b43fac2d71a2db85102e2ac
Narek Kolyan <nqolyan@gmail.com>
2022-03-10 15:08:17 +0400

    change footer html structure

--- src/containers/Footer/index.jsx
+++ src/containers/Footer/index.jsx
@@ -1,7 +1,5 @@
 import React, { useState } from 'react';
 import classNames from 'classnames';
-
-import { useWindowSize } from 'hooks/index';
 import {
   LOCATION_LINK,
   PHONE_NUMBER_LINK,
@@ -10,9 +8,7 @@ import {
   LINKEDIN_LINK,
   INSTAGRAM_LINK,
 } from 'constants/index';
-
 import styles from './Footer.scss';
-
 import {
   FbIcon,
   MailIcon,
@@ -23,16 +19,17 @@ import {
   InstagramIcon,
 } from '../../icons';
 import { NextLink } from '../../components';
-
 const FooterContainer = () => {
-  const { isMobile } = useWindowSize();
   const [isOpen, setIsOpen] = useState(false);
   const isOpenHandler = () => setIsOpen(!isOpen);
   const targetBlank = '_blank';
-
   return (
     <footer className={styles.container}>
-      <div className={`container ${styles.wrapper}`}>
+      <div
+        className={classNames(`container ${styles.wrapper}`, {
+          [styles.wrapper_reverse]: isOpen,
+        })}
+      >
         {!isOpen && (
           <div className={styles.routes}>
             <div className={styles.routes__item}>
@@ -41,120 +38,82 @@ const FooterContainer = () => {
             <div className={styles.routes__item}>
               <ArrowIcon />
             </div>
-            <div className={styles.routes__item}>
+            <div
+              className={classNames(styles.routes__item, styles.arrow_rotate)}
+            >
               <ArrowIcon />
             </div>
           </div>
         )}
-
-        {!isMobile ? (
-          <>
-            <div className={styles.routes}>
-              <div
-                className={classNames(
-                  styles.routes__item,
-                  styles.routes__item_link,
-                )}
-              >
-                <NextLink href={LOCATION_LINK} target={targetBlank}>
-                  <LocationIcon />
-                </NextLink>
-              </div>
-              <div
-                className={classNames(
-                  styles.routes__item,
-                  styles.routes__item_link,
-                )}
-              >
-                <NextLink href={`tel:${PHONE_NUMBER_LINK}`}>
-                  <PhoneIcon />
-                </NextLink>
-              </div>
-              <div
-                className={classNames(
-                  styles.routes__item,
-                  styles.routes__item_link,
-                )}
-              >
-                <NextLink href={`mailto:${MAIL_LINK}`}>
-                  <MailIcon />
-                </NextLink>
-              </div>
-            </div>
-            <div className={styles.routes}>
-              <div className={styles.routes__item}>
-                <NextLink href={FACEBOOK_LINK} target={targetBlank}>
-                  <FbIcon />
-                </NextLink>
-              </div>
-              <div className={styles.routes__item}>
-                <NextLink href={INSTAGRAM_LINK} target={targetBlank}>
-                  <InstagramIcon />
-                </NextLink>
-              </div>
-              <div className={styles.routes__item}>
-                <NextLink href={LINKEDIN_LINK} target={targetBlank}>
-                  <LinkedinIcon />
-                </NextLink>
-              </div>
-            </div>
-          </>
-        ) : (
-          <>
-            <div
-              className={classNames(styles.wrapper__animation, {
-                [styles.wrapper__animation_active]: isOpen,
-              })}
-            >
-              <div className={styles.routes__item}>
-                <FbIcon />
-              </div>
-              <div className={styles.routes__item}>
-                <InstagramIcon />
-              </div>
-              <div className={styles.routes__item}>
-                <LinkedinIcon />
-              </div>
-              <div
-                className={classNames(
-                  styles.routes__item,
-                  styles.routes__item_link,
-                )}
-              >
-                <LocationIcon />
-              </div>
-              <div
-                className={classNames(
-                  styles.routes__item,
-                  styles.routes__item_link,
-                )}
-              >
-                <PhoneIcon />
-              </div>
-              <div
-                className={classNames(
-                  styles.routes__item,
-                  styles.routes__item_link,
-                )}
-              >
-                <MailIcon />
-              </div>
-            </div>
-            <div
-              className={classNames(styles.wrapper__switcher, {
-                [styles.wrapper__switcher_active]: isOpen,
-              })}
-            >
-              <ArrowIcon
-                onClick={isOpenHandler}
-                className={styles.wrapper__switcher_close}
-              />
-            </div>
-          </>
-        )}
+        <div
+          className={classNames(styles.routes, {
+            [styles.wrapper__animation]: isOpen,
+          })}
+        >
+          <div
+            className={classNames(
+              styles.routes__item,
+              styles.routes__item_link,
+            )}
+          >
+            <NextLink href={LOCATION_LINK} target={targetBlank}>
+              <LocationIcon />
+            </NextLink>
+          </div>
+          <div
+            className={classNames(
+              styles.routes__item,
+              styles.routes__item_link,
+            )}
+          >
+            <NextLink href={`tel:${PHONE_NUMBER_LINK}`}>
+              <PhoneIcon />
+            </NextLink>
+          </div>
+          <div
+            className={classNames(
+              styles.routes__item,
+              styles.routes__item_link,
+            )}
+          >
+            <NextLink href={`mailto:${MAIL_LINK}`}>
+              <MailIcon />
+            </NextLink>
+          </div>
+        </div>
+        <div
+          className={classNames(styles.routes, {
+            [styles.wrapper__animation]: isOpen,
+          })}
+        >
+          <div className={styles.routes__item}>
+            <NextLink href={FACEBOOK_LINK} target={targetBlank}>
+              <FbIcon />
+            </NextLink>
+          </div>
+          <div className={styles.routes__item}>
+            <NextLink href={INSTAGRAM_LINK} target={targetBlank}>
+              <InstagramIcon />
+            </NextLink>
+          </div>
+          <div className={styles.routes__item}>
+            <NextLink href={LINKEDIN_LINK} target={targetBlank}>
+              <LinkedinIcon />
+            </NextLink>
+          </div>
+        </div>
+        <div
+          className={classNames(styles.wrapper__switcher, {
+            [styles.wrapper__switcher_active]: isOpen,
+          })}
+        >
+          <ArrowIcon
+            onClick={isOpenHandler}
+            className={styles.wrapper__switcher_close}
+          />
+        </div>
       </div>
     </footer>
   );
 };
-
 export default FooterContainer;
