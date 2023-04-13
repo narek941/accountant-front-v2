7662f01228a90f09fd83202a67dd1ba1805d54e5
hrant.abrahamyan.imshop <hrant.abrahamyan.imshop@gmail.com>
2023-04-13 12:07:56 +0400

    update next

---
+++ mail/mailTransporter.js
@@ -0,0 +1,15 @@
+// import { createTransport } from 'nodemailer';
+
+// import { MAIL_SENDER_EMAIL, MAIL_SENDER_PASS } from '../constants/index';
+
+// const mailTransporter = createTransport({
+//   host: 'smtp.mail.ru',
+//   port: 465,
+//   secure: true,
+//   auth: {
+//     user: MAIL_SENDER_EMAIL,
+//     pass: MAIL_SENDER_PASS,
+//   },
+// });
+
+// export default mailTransporter;
